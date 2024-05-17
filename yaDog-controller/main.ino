a3867ae376e8aebe410e8ed94fb3b7d0d7a93939
Kunqi Zhao <110202952+ZhaoKunqi@users.noreply.github.com>
2024-05-17 15:50:57 +0800

    Update main.ino

--- yaDog-controller/main.ino
+++ yaDog-controller/main.ino
@@ -5,8 +5,13 @@
 String cacheString = "";
 bool cacheStringComplete = false;
 void setup() {
-  delay(1000);
+  delay(500);
   Serial.begin(9600);
+  pinMode(PWR_PIN, OUTPUT);
+  pinMode(RST_PIN, OUTPUT);
+  delay(500);
+  digitalWrite(PWR_PIN, 0);
+  digitalWrite(RST_PIN, 0);
 }
 
 void loop() {
@@ -22,10 +27,31 @@ void serialEvent() {
   }
 }
 
+//Relay Actions Section
 void executeBootEvent(){
-  pass();
+  delay(100);
+  digitalWrite(PWR_PIN, 1);
+  delay(300);
+  digitalWrite(PWR_PIN, 0);
+  delay(100);
 }
 
-void executeRebootEvent(){
-  pass();
+void executeSoftRebootEvent(){
+  delay(100);
+  digitalWrite(RST_PIN, 1);
+  delay(300);
+  digitalWrite(RST_PIN, 0);
+  delay(100);
+}
+
+void executeHardRebootEvent(){
+  delay(100);
+  digitalWrite(PWR_PIN, 1);
+  delay(6000);
+  digitalWrite(PWR_PIN, 0);
+  delay(1000);
+  digitalWrite(PWR_PIN, 1);
+  delay(300);
+  digitalWrite(PWR_PIN, 0);
+  delay(100);
 }
